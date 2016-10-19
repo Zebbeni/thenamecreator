ef425af675ecc3a1f32f3b92077f7ab3b3f992ce
Zebbeni <andrew.albers@gmail.com>
2016-10-18 23:16:41 -0600

    removed comments, react stuff

--- main.go
+++ main.go
@@ -5,77 +5,12 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
-	"time"
-
-	"google.golang.org/appengine"
-	"google.golang.org/appengine/datastore"
-	"golang.org/x/net/context"
 )
 
-type comment struct {
-	ID     int64  `json:"id"`
-	Author string `json:"author"`
-	Text   string `json:"text"`
-}
-
-// commentKey returns the key used for all guestbook entries.
-func commentKey(c context.Context) *datastore.Key {
-	// The string "default_guestbook" here could be varied to have multiple guestbooks.
-	return datastore.NewKey(c, "comment", "comment_thread", 0, nil)
-}
-
-// Handle comments
-func handleComments(w http.ResponseWriter, r *http.Request) {
-
-	c := appengine.NewContext(r)
-	switch r.Method {
-	case "GET":
-		// Decode the JSON data
-		query := datastore.NewQuery("comment").Order("ID").Limit(10)
-		// query := datastore.NewQuery("comment").Limit(10)
-		comments := make([]comment, 0, 10)
-		if _, err := query.GetAll(c, &comments); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		// Marshal the comments to indented json.
-		commentData, err := json.MarshalIndent(comments, "", "    ")
-		if err != nil {
-			http.Error(w, fmt.Sprintf("Unable to marshal comments to json: %s", err), http.StatusInternalServerError)
-			return
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.Header().Set("Cache-Control", "no-cache")
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		io.Copy(w, bytes.NewReader(commentData))
-
-	case "POST":
-		newcomment := comment{
-			ID:     time.Now().UnixNano() / 1000000,
-			Author: r.FormValue("author"),
-			Text:   r.FormValue("text"),
-		}
-		key := datastore.NewIncompleteKey(c, "comment", commentKey(c))
-		_, err := datastore.Put(c, key, &newcomment)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		return
-
-	default:
-		http.Error(w, fmt.Sprintf("Unsupported method: %s", r.Method), http.StatusMethodNotAllowed)
-	}
-}
-
 func init() {
-	http.HandleFunc("/api/comments", handleComments)
 	http.HandleFunc("/signin", signin)
 	http.Handle("/", http.FileServer(http.Dir("./public")))
 }
@@ -93,16 +28,3 @@ func main() {
 		log.Fatal(err)
 	}
 }
-
-// func handleLogin(w http.ResponseWriter, r *http.Request) {
-// 	w.Header().Set("Content-type", "text/html; charset=utf-8")
-// 	ctx := appengine.NewContext(r)
-// 	u := user.Current(ctx)
-// 	if u == nil {
-// 		url, _ := user.LoginURL(ctx, "/")
-// 		fmt.Fprintf(w, `%s`, url)
-// 		return
-// 	}
-// 	url, _ := user.LogoutURL(ctx, "/")
-// 	fmt.Fprintf(w, `%s`, u, url)
-// }
