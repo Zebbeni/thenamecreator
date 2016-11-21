83b0e3e3d25c2dfc8e13ac9e583b82fec297ebaa
Zebbeni <andrew.albers@gmail.com>
2016-11-20 22:54:40 -0700

    cleaned up signin flow

--- public/scripts/account.js
+++ public/scripts/account.js
@@ -2,16 +2,17 @@
  * Created by andrewalbers on 11/5/16.
  */
 
+var profile = undefined;
 var username = undefined;
 
 var onSignIn = function(googleUser) {
     var id_token = googleUser.getAuthResponse().id_token;
-    var profile = googleUser.getBasicProfile();
+    profile = googleUser.getBasicProfile();
+    username = profile.getGivenName();
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
     console.log('Name: ' + profile.getGivenName());
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail());
-    username = profile.getGivenName();
     var xhr = new XMLHttpRequest();
     xhr.open('POST', '/signin');
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
@@ -20,13 +21,29 @@ var onSignIn = function(googleUser) {
         console.log("id_token:", id_token);
     };
     xhr.send('idtoken=' + id_token);
-    $('a#account-name').text(username);
+    $('a#signin-a').text("");
+    $('a#profile-a').text(username + " ");
+    $('a#signout-a').text("(Sign Out)");
+    closeSignInOptions();
 };
 
 var signOut = function() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
-        console.log('User signed out.');
-        $('a#account-name').text("");
+        $('a#signin-a').text("Sign In");
+        $('a#profile-a').text("");
+        $('a#signout-a').text("");
     });
+};
+
+var showSignIn = function() {
+    $('#signin-div').show();
+};
+
+var closeSignInOptions = function() {
+    $('#signin-div').hide();
+};
+
+var showProfile = function() {
+    console.log("show profile placeholder");
 };
